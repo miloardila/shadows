d74b51ffdf50b780d24a1fdabe9fe51f2ff1867b
Unknown <daniel.montes@upb.edu.co>
2017-11-03 07:17:16 -0500

    UI

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+    [SerializeField]
+    private int moveTo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(moveTo);
+    }
+}
---
+++ Assets/Scripts/MoveScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour {
+
+    private float time;
+    [SerializeField]
+    private int moveTo;
+
+	// Use this for initialization
+	void Start () {
+        time = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time = time + Time.deltaTime;
+        if (time >= 5)
+        {
+            SceneManager.LoadScene(moveTo);
+        }     
+	}
+}
